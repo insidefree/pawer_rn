4ba6a60ad730012f1d2f55a628e44ee24903c137
Denis Sorokin <denys.sorokin@gmail.com>
2017-12-12 13:31:11 +0200

    Started pawslist

--- src/navigation/screens/PawListScreen.js
+++ src/navigation/screens/PawListScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, FlatList, StyleSheet } from 'react-native'
 
+/* components */
+import Header from '../../components/Header'
+import PawCard from '../../components/PawCard'
 
 export default class PawListScreen extends Component {
     static navigationOptions = {
@@ -15,7 +18,24 @@ export default class PawListScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>List of animals</Text>
+                <Header title={'Pawslist'}/>
+                <FlatList
+                    data={[
+                        { key: 'Devin', at: true, care: false },
+                        { key: 'Jackson', at: false, care: true },
+                        { key: 'James', at: false, care: false },
+                        { key: 'Joel', at: false, care: true },
+                        { key: 'John', at: true, care: false },
+                        { key: 'Jillian', at: true, care: false },
+                        { key: 'Jimmy', at: true, care: false },
+                        { key: 'Julie', at: false, care: false },
+                    ]}
+                    renderItem={({ item }) => <PawCard
+                        name={item.key}
+                        attention={item.at}
+                        care={item.care}
+                    />}
+                />
             </View>
         )
     }
@@ -25,7 +45,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'stretch',
+        backgroundColor: '#FFF',
+        paddingLeft: 20,
     },
     icon: {
         marginTop: 15,
